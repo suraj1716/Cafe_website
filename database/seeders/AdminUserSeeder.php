78eaf1a174dd162549e41de55ed1c700e1dea1b3
unknown <shrestha.suraj.2013@gmail.com>
2025-10-23 03:16:48 +1100

    docker file added and fixed build

--- database/seeders/AdminUserSeeder.php
+++ database/seeders/AdminUserSeeder.php
@@ -7,21 +7,19 @@ use Illuminate\Database\Seeder;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 
-
-
 class AdminUserSeeder extends Seeder
 {
-
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-         User::create([
-    'name' => 'Admin',
-    'email' => 'shrestha.suraj.2013@gmail.com',
-    'password' => Hash::make('qwerty123'),
-]);
-        //
+        User::firstOrCreate(
+            ['email' => 'shrestha.suraj.2013@gmail.com'], // check if admin exists
+            [
+                'name' => 'Admin',
+                'password' => Hash::make('qwerty123'), // only set if new
+            ]
+        );
     }
 }
